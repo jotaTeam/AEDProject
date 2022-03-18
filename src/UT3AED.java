66dae8d55f8d15cd6dfd5f582309fb4ddcafc0b3
wenesmek <wenesmek@gmail.com>
2022-03-18 04:12:49 +0000

    Structure done

--- src/UT3AED.java
+++ src/UT3AED.java
@@ -1,10 +1,12 @@
+import Controllers.Controller;
+
 public class UT3AED {
 
 
     public static void main(String[] args) {
 
 
-        System.out.println("Hello world");
+        new Controller();
 
 
     }
