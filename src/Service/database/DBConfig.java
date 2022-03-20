2aed066662c401ce25ff0511ede80d8c399e048c
AdrielAO <adrielarochaoronoz@gmail.com>
2022-03-20 08:40:40 +0000

    cositas

--- src/Service/database/DBConfig.java
+++ src/Service/database/DBConfig.java
@@ -1,2 +1,28 @@
-package Service.database;public class DBConfig {
+package Service.database;
+
+import java.util.Properties;
+
+public class DBConfig {
+    private static final String USERNAME = "adri";
+    private static final String PASSWORD = "1234";
+    private static final String TABLE = "pruebaAED";
+    public static final String URL = "jdbc:mysql://localhost";
+    private static final String PORT= "3306";
+
+
+
+
+    public static Properties getProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("password", PASSWORD);
+        properties.setProperty("user", USERNAME);
+        properties.setProperty("url", URL);
+        properties.setProperty("port", PORT);
+
+        System.getenv().get("");
+
+        return properties;
+    }
+
 }
+
--- src/Service/database/DBConnection.java
+++ src/Service/database/DBConnection.java
@@ -1,2 +1,35 @@
-package Service.database;public class DBConnection {
+package Service.database;
+
+import java.sql.*;
+import java.sql.DriverManager;
+
+import static Service.database.DBConfig.*;
+
+
+
+public class DBConnection {
+
+    private static Connection connection;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            new DBConnection();
+        }
+        System.out.println("connect to DB");
+        return connection;
+    }
+
+    private DBConnection(){
+        try {
+            this.connection = DriverManager.getConnection(URL,getProperties());
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
+
