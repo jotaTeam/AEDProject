470345cda8f8617ed5a5875d54f98d36b19bd6fe
abigailojeda <abigail.arte@gmail.com>
2022-03-20 01:03:19 +0000

    home

---
+++ src/Views/Home.java
@@ -0,0 +1,22 @@
+package Views;
+
+import javax.swing.*;
+
+public class Home extends JFrame{
+    private JPanel panel1;
+
+
+    public Home(){
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(935, 845);
+        setResizable(false);
+
+        setLocationRelativeTo(null);
+
+        add(panel1);
+
+    }
+
+}
